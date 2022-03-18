69215321f668e683937b2a0d14c9a5e1b78b1bcb
Xander Harrison <xander.harrison77@gmail.com>
2022-03-18 14:52:09 -0400

    UI Update

--- ui/scripts/components/kennel/kennel_create_form.js
+++ ui/scripts/components/kennel/kennel_create_form.js
@@ -6,10 +6,14 @@ const KennelCreateForm = {
   },
   template: `
     <div class="kennel_create_form">
-      <div class="kennel_create_form_header">KENNEL CREATOR</div>
+      <div class="kennel_create_form_header">
+        KENNEL CREATOR
+        <div class="kennel_create_form_header_close">X</div>
+      </div>
       <div class="kennel_create_form_body">
         <kennel-create-input label="Name" placeholder="K9's Name" />
         <hr style="width: 310px;" />
+        <kennel-create-dropdown label="Genders" :items="items" />
         <kennel-create-dropdown label="Genders" :items="items" />
       </div>
     </div>
--- ui/scripts/views/kennel.js
+++ ui/scripts/views/kennel.js
@@ -9,11 +9,15 @@ const Kennel = {
   },
   template: `
     <div class="kennel_container">
-      <div class="kennel_header">KENNEL</div>
+      <div class="kennel_header">
+        KENNEL
+        <div class="kennel_header_creator_open" v-show="!showCreator">X</div>
+      </div>
       <div class="kennel_body">
         <kennel-card v-for="(dog, dogIndex) in dogs" :dog="dog"></kennel-card>
       </div>
-      <kennel-create-form v-show="showCreator"></kennel-create-form>
+      <div class="kennel_container_grayscale" v-show="showCreator"></div>
+      <kennel-create-form :class="{ showCreator: 'kennel_create_form_animate_in' }" v-show="showCreator"></kennel-create-form>
     </div>
   `,
   components: { KennelCard, KennelCreateForm }
